3121e536df44a62bd83f1d91bd1051289ae3ff7f
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-10 22:40:18 +0200

    add table view

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -3,71 +3,29 @@ import styles from './Tables.module.scss';
 import { Link } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
-
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
 
 import AddIcon from '@material-ui/icons/Add';
 import Divider from '@material-ui/core/Divider';
+import Toolbar from '@material-ui/core/Toolbar';
 
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
-  //Appointments,
-  // AppointmentTooltip,
-  //DateNavigator,
-  //TodayButton,
+  Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-import Toolbar from '@material-ui/core/Toolbar';
-
-
-
-const demoContent = [
-  {hour: '12:00', table: 1, tableStatus:'booked'},
-  {hour: '12:30', table: null, tableStatus: null},
-  {hour: '13:00', tableStatus: null},
-  {hour: '13:30', tableStatus: 'event'},
-];
-
-const tables = [
-  {id: 1},
-  {id: 2},
-  {id: 3},
-  {id: 4},
-];
-
+const currentDate = '2019-06-23' ;
 const data = [
-  { startDate: '2020-01-28 15:00', endDate: '2020-01-28 16:00', title: 'John Smith, table 1' },
-  { startDate: '2020-01-28 18:00', endDate: '2020-01-28 19:30', title: 'Andrea Sun, table 2' },
-  { startDate: '2020-01-28 14:00', endDate: '2020-01-28 16:00', title: 'Lesley Nilson, table 3' },
-  { startDate: '2020-01-28 15:00', endDate: '2020-01-28 17:30', title: 'Miranda Ball, table 2' },
-  { startDate: '2020-01-28 19:00', endDate: '2020-01-28 20:30', title: 'Carl Fischer, table 1' },
+  { title: 'Kaja Nowak, table 1, 4ppl',   startDate: '2019-06-23T20:00', endDate: '2019-06-23T21:30' },
+  { title: 'Anna Łokieć, table 3, 6ppl',  startDate: '2019-06-23T21:00', endDate: '2019-06-23T22:00' },
+  { title: 'Franek Bobek, table 4, 2ppl', startDate: '2019-06-23T12:30', endDate: '2019-06-23T14:30' },
+  { title: 'Event 1 , table 3',           startDate: '2019-06-23T18:00', endDate: '2019-06-23T20:00' },
+  { title: 'Event 2, table 2',            startDate: '2019-06-23T14:30', endDate: '2019-06-23T19:30' },
 ];
 
-const renderActions = status => {
-  switch (status) {
-    case 'free':
-      return (
-        <>
-          <Button>booked</Button>
-          <Button>free</Button>
-        </>
-      );
-    case 'paid':
-      return (
-        <Button>free</Button>
-      );
-    default:
-      return null;
-  }
-};
 
 const Tables = (id) => {
   return (
@@ -87,60 +45,22 @@ const Tables = (id) => {
         </div>
         <Divider/>
         <div  className={styles.heading}>
-
-
-
-
-          <Scheduler
-            data={data}
-          >
-            <ViewState
-              defaultCurrentDate={new Date()}/>
-            <DayView
-              startDayHour={12}
-              endDayHour={24}
-            />
-            <Toolbar />
-            {/*} <DateNavigator />
-            <TodayButton />
-            <Appointments />
-  <AppointmentTooltip />*/}
-          </Scheduler>
-
-
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Hour/Table</TableCell>
-                {tables.map(table => (
-                  <TableCell key={table.id}>{table.id}</TableCell>
-                ))}
-                <TableCell>Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {demoContent.map(row => (
-                <TableRow key={row.id}>
-                  <TableCell component="th" scope="row">
-                    {row.hour}
-                  </TableCell>
-                  <TableCell>
-                    {row.table}
-                  </TableCell>
-                  <TableCell>
-                    {row.tableStatus && (
-                      <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
-                        {row.order}
-                      </Button>
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    {renderActions(row.status)}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <h4>Reservations for today</h4>
+          <Paper>
+            <Scheduler
+              data={data}
+            >
+              <ViewState
+                currentDate={currentDate}
+              />
+              <DayView
+                startDayHour={12}
+                endDayHour={24}
+              />
+              <Appointments />
+            </Scheduler>
+          </Paper>
+          <Toolbar />
         </div>
       </Paper>
     </Container>
